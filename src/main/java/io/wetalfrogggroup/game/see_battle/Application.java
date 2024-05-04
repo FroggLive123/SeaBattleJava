f61699d00c0edb89a6503b06541fdd17088a532a
silentium.noxe <silentium.noxe@gmail.com>
2024-05-04 23:13:26 +0300

    enable online

--- src/main/java/io/wetalfrogggroup/game/see_battle/Application.java
+++ src/main/java/io/wetalfrogggroup/game/see_battle/Application.java
@@ -2,6 +2,9 @@ package io.wetalfrogggroup.game.see_battle;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.wetalfrogggroup.game.see_battle.firebase.FirebaseApplication;
+import io.wetalfrogggroup.game.see_battle.online.FirestoreConnection;
+import io.wetalfrogggroup.game.see_battle.online.HealthPub;
 import io.wetalfrogggroup.game.see_battle.util.CloseableRegister;
 import lombok.SneakyThrows;
 
@@ -14,10 +17,10 @@ class Application {
 
         try {
             //todo: uncomment this block to enable online mode
-            //            new FirebaseApplication();
-            //            var hp = new HealthPub(new FirestoreConnection());
-            //            cr.register(hp);
-            //            hp.start();
+            new FirebaseApplication();
+            var hp = new HealthPub(new FirestoreConnection());
+            cr.register(hp);
+            hp.start();
 
             Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
             config.setTitle("Sea Battle");
